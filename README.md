805396b8156ecb9ede3cf4ce4c9affdcec08ee75
Marcello <hey@marcello.gg>
2025-08-02 12:25:17 -0400

    feat: initial version of bonobo (#1)
    * feat: string helper utils
    
    * Update README.md
    
    * feat: split utility helpers for maintainence
    
    * feat: turn the package to public
    
    * feat: make the package public

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # bonobo.js
 🙊 A civilized and peaceful utility library for TypeScript and ECMAScript.
+
+## Installation
+
+```sh
+npm install --save @bytebury/bonobo
+```
+
+## Sample Usage
+
+```js
+import { reverse, isNullOrWhitespace, titleCase, bool } from "@bytebury/bonobo"
+
+function isPalindrome(text) {
+  return reverse(text) === text;
+}
+
+function sayHello(name) {
+  if (isNullOrWhitespace(name)) {
+    console.log("Hello, Guest.");
+    return;
+  }
+  console.log(`Hello, ${titleCase(name)}`);
+}
+
+// Sometimes, APIs will return booleans as text (for whatever reason).
+function legacyApi() {
+  const user = { name: "Baboon", isAdmin: "false" };
+
+  if (bool(user.isAdmin)) { // this is false
+    console.log("User is an admin!");
+    return
+  }
+  console.log("User is not an admin!"); // in this example this outputs!
+}
+
+
+```
---
+++ src/core/core.test.ts
@@ -0,0 +1,102 @@
+import { describe, expect, test } from "vitest";
+import { bool, isEmpty, isNull, reverse, stringify } from "./core";
+
+describe("#stringify", () => {
+	test("should stringify a primitive to a string", () => {
+		expect(stringify(123)).toBe("123");
+		expect(stringify(true)).toBe("true");
+		expect(stringify(null)).toBe("null");
+	});
+
+	test("it should stringify objects and strings as JSON strings", () => {
+		expect(stringify([])).toBe("[]");
+		expect(stringify({ foo: "bar", baz: 1 })).toBe(
+			JSON.stringify({ foo: "bar", baz: 1 }),
+		);
+	});
+});
+
+describe("#bool", () => {
+	test('should return false when the string is "false", case insensitive', () => {
+		expect(bool("false")).toBe(false);
+		expect(bool("FALSE")).toBe(false);
+		expect(bool(" FALSE ")).toBe(false);
+	});
+
+	test('should return false when the string is "0"', () => {
+		expect(bool("0")).toBe(false);
+	});
+
+	test("should return false when the string is null or whitespace", () => {
+		expect(bool("null")).toBe(false);
+		expect(bool("undefined")).toBe(false);
+		expect(bool("   ")).toBe(false);
+	});
+
+	test("should return false for empty lists", () => {
+		expect(bool([])).toBe(false);
+	});
+
+	test("should return true for anything else", () => {
+		expect(bool("1")).toBe(true);
+		expect(bool("mike")).toBe(true);
+		expect(bool("true")).toBe(true);
+		expect(bool(["123"])).toBe(true);
+	});
+});
+
+describe("#reverse", () => {
+	test('should return "elppa" for "apple"', () => {
+		expect(reverse("apple")).toBe("elppa");
+	});
+
+	test("should maintain whitespace and capitalization", () => {
+		expect(reverse("Hello World!")).toBe("!dlroW olleH");
+	});
+
+	test("should reverse the list", () => {
+		expect(reverse([1, 2, 3])).toStrictEqual([3, 2, 1]);
+	});
+});
+
+describe("#isEmpty", () => {
+	test('should return true when the string is ""', () => {
+		expect(isEmpty("")).toBe(true);
+	});
+
+	test('should return false when the string is whitespace " "', () => {
+		expect(isEmpty(" ")).toBe(false);
+	});
+
+	test("should return true when the list is empty", () => {
+		expect(isEmpty([])).toBe(true);
+	});
+
+	test("should return false when the list is not empty", () => {
+		expect(isEmpty([0])).toBe(false);
+	});
+});
+
+describe("#isNull", () => {
+	test('should return true when the string is "null"', () => {
+		expect(isNull("null")).toBe(true);
+	});
+
+	test("should return true when the string is `null`", () => {
+		expect(isNull(null)).toBe(true);
+	});
+
+	test("should return true when the string is `undefined`", () => {
+		expect(isNull(undefined)).toBe(true);
+	});
+
+	test('should return true when the string is "undefined"', () => {
+		expect(isNull("undefined")).toBe(true);
+	});
+
+	test("should return false when the thing is not null or undefined", () => {
+		expect(isNull([])).toBe(false);
+		expect(isNull({})).toBe(false);
+		expect(isNull("something else")).toBe(false);
+	});
+});
